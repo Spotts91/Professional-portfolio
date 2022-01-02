8d4cb6e09fd6a70637373bebc630e9dce49cefa0
Spotts91 <oakshassa864@gmail.com>
2022-01-02 13:15:52 -0800

    Index tweaks and experimental JS coding

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+    // sticky navbar on sroll script
+    if(this.scrollY > 20){
+        $('navbar').addClass("sticky");
+            }else{
+                $('.navbar').removeClass("sticky");
+            }
+
+            // scroll-up button show/hide script
+            if(this.scrollY > 500){
+                $('.scroll-up-btn').addClass("show");
+            }else{
+                $('.scroll-up-btn').removeClass("show");
+            }
+    });
+
+    // slidie-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        //remove smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('..navbar.menu li a').click(function(){
+        //reapplying smooth scroll on menu item click
+        $('.html').css("scrollBehavior", "smooth");
+    });
+
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar.menu').toggleClass("active");
+        $('.menu-btn-img').toggleClass("active");
+    });
+    //Typing text animation script
+})                                                              
